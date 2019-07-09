0026d30164a7eb4e504e8f58e1b50b1809d6f6df
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2019-07-09 16:46:28 -0300

    startup-tdx-torizon.sh: do everything specific to Torizon
    Instead of hardcoding URLs or anything like that in the Dockerfile, add
    as a helper script.

--- startup-tdx-torizon.sh
+++ startup-tdx-torizon.sh
@@ -1,22 +1,78 @@
-cd ~/torizon && ~/bin/repo sync
-echo "You have to accept freescale EULA. Read it carefully and then accept it."
-sleep 3
-cat ~/torizon/layers/meta-freescale/EULA
-read -p "Do you accept the EULA? [y/n] " yn
-case $yn in
-    [Yy]* ) echo 'EULA accepted';;
-    [Nn]* ) exit;;
-    * ) echo "Please answer yes or no." && exit ;;
-esac
-cd ~/torizon
+#!/bin/bash
+
+WDIR=/workdir
+DISTVAL=torizon
+
+# Check if default build directory is setup
+if [ -z "$1" ]
+then
+    BDDIR=build-torizon
+else
+    BDDIR="$1"
+fi
+
+# Check if branch is passed as argument
+if [ -z "$BRANCH" ]
+then
+    BRANCH=master
+fi
+
+# Configure Git if not configured
+if [ ! $(git config --global --get user.email) ]; then
+    git config --global user.email "you@example.com"
+    git config --global user.name "Your Name"
+    git config --global color.ui false
+fi
+
+# Create a directory for Torizon
+mkdir -p $WDIR/$DISTVAL
+cd $WDIR/$DISTVAL
+
+# Initialize if repo not yet initialized
+repo status 2> /dev/null
+if [ "$?" = "1" ]
+then
+    repo init -u https://github.com/toradex/toradex-torizon-manifest -b $BRANCH
+    repo sync
+fi # Do not sync automatically if repo is setup already
+
+# Initialize build environment
 MACHINE=$MACHINE source setup-environment
-echo 'ACCEPT_FSL_EULA="1"' >> ~/torizon/build-torizon/conf/local.conf
-cd ~/torizon/build-torizon
+
+# Accept Freescale/NXP EULA
+if ! grep -q ACCEPT_FSL_EULA $WDIR/$DISTVAL/$BDDIR/conf/local.conf
+then
+    echo 'You have to accept freescale EULA. Read it carefully and then accept it.'
+    echo 'Press "space" to scroll down and "q" to exit'
+    sleep 3
+    less $WDIR/$DISTVAL/layers/meta-freescale/EULA
+    while true; do
+        read -p "Do you accept the EULA? [y/n] " yn
+        case $yn in
+            [Yy]* ) echo 'EULA accepted'
+                echo 'ACCEPT_FSL_EULA="1"' >> $WDIR/$DISTVAL/$BDDIR/conf/local.conf
+                break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done    
+fi
+
+# Create image_list.json for Toradex Easy Installer
+if [ ! -f $WDIR/$DISTVAL/$BDDIR/image_list.json ]
+then
+    cp /etc/image_list.json $WDIR/$DISTVAL/$BDDIR/image_list.json
+fi
+
+# Only start build if requested
 if [ -z "$TARGET" ]
 then
-    echo "> bitbake torizon-core-docker"
-    bitbake torizon-core-docker
+    echo "Build enviorment configured"
 else
+    echo "Build enviorment configured. Building target image $TARGET"
     echo "> bitbake $TARGET"
     bitbake $TARGET
 fi
+
+# Spawn a shell
+exec bash -i
